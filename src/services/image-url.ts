34b389befc8a5c65e795d34a52c75330d81416be
SkiopCx <87648250+SkiopCx@users.noreply.github.com>
2025-06-20 00:36:19 +0200

    Add getCropImage

---
+++ src/services/image-url.ts
@@ -0,0 +1,8 @@
+// 提取小的图片来加速加载
+const getCropImageUrl = (url: string) => {
+    const target = 'media/';
+    const index = url.indexOf(target) + target.length;
+    return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
+}
+
+export default getCropImageUrl;
