0bc8a05bb6954867cd7b5c4980fe185e43ad9f76
SkiopCx <87648250+SkiopCx@users.noreply.github.com>
2025-06-21 00:52:14 +0200

    Display image of genre

--- src/compoments/GenreList.tsx
+++ src/compoments/GenreList.tsx
@@ -1,13 +1,26 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres, { type Genre } from "../hooks/useGenres";
+import getCropImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
-      {data.map((genre) => (
-        <li>{genre.name}</li>
-      ))}
-    </ul>
+    <>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCropImageUrl(genre.image_background)}
+              ></Image>
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
